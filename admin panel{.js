b6f504ea2d4b8b0162a6d5f873c96a4091f29e36
paras-mahajann <mahajanparas912@gmail.com>
2026-02-26 23:14:53 +0530

    starting with backend

---
+++ admin panel{.js
@@ -0,0 +1,10 @@
+admin panel{
+    questionlist -> question_push to user panel (30s)
+    with team id -> list of submitted team
+}
+Dashboard -> 1)current question id -> total pot value   2)statistic team top 5
+user panel{
+    login ->teamId,password;
+    current balance 
+    after you push->display (Option,Investment tier)
+}

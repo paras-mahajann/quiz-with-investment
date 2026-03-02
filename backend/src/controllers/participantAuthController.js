1062dcaf4c110397366ce9b73b38dca07e4e1079
paras-mahajann <mahajanparas912@gmail.com>
2026-03-02 15:10:16 +0530

    fix cors

--- backend/src/controllers/participantAuthController.js
+++ backend/src/controllers/participantAuthController.js
@@ -33,8 +33,9 @@ const loginController = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      sameSite: "strict",
-      maxAge: 60 * 60 * 1000
+      secure:true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000
     });
 
     res.status(200).json({
